df09bb617f56a8b19708ae7303b39936d1e967c5
Minji Kim <minji9360@gmail.com>
2022-01-25 19:47:02 +0900

    feat: cursor에 물고기 아이콘 추가
    - 물고기는 임시 이미지
    - cursor class 자체에 z-index가 적용되지 않아 cursor 내 각 class에 z-index 적용

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-let mouseCursor = document.querySelector(".cursor");
+let cursorPointer = document.querySelector(".cursor__pointer");
+let cursorImage = document.querySelector(".cursor__image");
 
 window.addEventListener("scroll", cursor);
 window.addEventListener("mousemove", cursor);
 
 // 커스텀한 커서의 left&top값을 커서의 XY좌표값과 일치시킴
 function cursor(e) {
-    mouseCursor.style.left = e.pageX + "px";
-    mouseCursor.style.top = e.pageY - scrollY + "px";
+    cursorPointer.style.left = e.pageX + "px";
+    cursorPointer.style.top = e.pageY - scrollY + "px";
+    cursorImage.style.left = e.pageX + 6 + "px";
+    cursorImage.style.top = e.pageY - scrollY - 4 + "px";
 }
